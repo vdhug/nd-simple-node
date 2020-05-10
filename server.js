ae51d43aceffafa5ddddeb41755c8cf15e48af82
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2020-05-10 17:29:52 -0500

    Small change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ function sleep(ms) {
   
   async function main() {
     while(true) {
-      console.log('Containers rule!');
+      console.log('Containers rule! Simple Node');
       await sleep(5000);
     }
   }
