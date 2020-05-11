d3ececdb13ccbf8ddaa80483cfac63f90f1f0304
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2020-05-11 18:23:55 -0500

    Using env variables

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   
-  async function main() {
-    while(true) {
-      console.log('Containers rule! Simple Node');
-      await sleep(5000);
-    }
+async function main() {
+  while(true) {
+    console.log('Containers rule! Simple Node');
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(5000);
   }
+}
   
-  main();
+main();
